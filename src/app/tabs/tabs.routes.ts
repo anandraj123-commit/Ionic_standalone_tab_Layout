a10e89f8001e75a3358e6e0220d5cb64c6ada500
Anand Raj <anandraj7008@gmail.com>
2025-03-30 20:17:31 +0530

    my chnages

--- src/app/tabs/tabs.routes.ts
+++ src/app/tabs/tabs.routes.ts
@@ -36,7 +36,7 @@ export const routes: Routes = [
       {
         path: 'tab2',
         loadComponent: () =>
-          import('../tab2/tab2.page').then((m) => m.Tab2Page),
+          import('./tab2/tab2.page').then((m) => m.Tab2Page),
       },
       {
         path: 'tab3',
